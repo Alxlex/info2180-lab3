b9a8e10b375a386aeecb421292fd35387e771aa9
Alxlex <salmon.alex235@gmail.com>
2022-10-07 00:44:20 -0500

    The game execution

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,36 @@
-window.onload = bg
-
-function bg ()
+window.onload = begin
+var box
+const arr = ["","","","","","","","",""]
+wrong = true
+function begin ()
 {
-    var box = document.getElementById("board").children
+    box = document.getElementById("board").children
     
     
     for (let bx of box){
         bx.classList.add("square")
+        bx.addEventListener('click',start)
         console.log(bx)
     }
+}
+
+function start(event)
+{
+   index = Array.from(box).indexOf(event.target)
+
+   if (wrong == true)
+   {
+    box[index].innerHTML = "X";
+    box[index].classList.add('X')
+    wrong = false
+   } else {
+    box[index].innerHTML = "O"
+    box[index].classList.add('O')
+    wrong = true
+   }
+
+     arr[index]= box[index].innerHTML
+
+
+   console.log(index)
 }
